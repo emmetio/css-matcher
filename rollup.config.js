b1f857b020d9f6ddb0f55cd356a4aad8d1e65cc2
Sergey Chikuyonok <serge.che@gmail.com>
2019-10-27 01:06:37 +0300

    Added builder

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import typescript from 'rollup-plugin-typescript2';
+import nodeResolve from 'rollup-plugin-node-resolve';
+
+export default {
+    input: './src/index.ts',
+    plugins: [nodeResolve(), typescript({
+        tsconfigOverride: {
+            compilerOptions: { module: 'esnext' }
+        }
+    })],
+    output: [{
+        file: './dist/css-matcher.es.js',
+        format: 'es',
+        sourcemap: true
+    }, {
+        file: './dist/css-matcher.cjs.js',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true
+    }]
+};
